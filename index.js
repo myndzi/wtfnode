99b222c6d6313736d54fbe565efd4af71c8c91ac
Kris Reeves <kris.re@bbhmedia.com>
2016-04-10 17:08:02 -0400

    Add a bunch of new types (child process, specific server information, etc.) Capture timer callbacks differently, support old and new nodes

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 
 'use strict';
 
-var Socket, Server, Timer;
+var Socket, dgramSocket, Server, TlsServer, HttpServer, HttpsServer, Timer, ChildProcess;
 
 function timerCallback(thing) {
+    if (typeof thing._repeat === 'function') { return '_repeat'; }
     if (typeof thing._onTimeout === 'function') { return '_onTimeout'; }
     if (typeof thing._onImmediate === 'function') { return '_onImmediate'; }
 }
@@ -29,53 +30,89 @@ function timerCallback(thing) {
         }
     });
 
-    // timers call this
-    var L = require('_linklist');
-    var _L_append = L.append;
-    L.append = function (list, item) {
-        if (list instanceof Timer || (typeof list === 'object' && list.hasOwnProperty('_idleNext'))) {
-            if (item && timerCallback(item)) {
-                var stack = __stack;
-                for (var i = 5; i < stack.length; i++) {
+    var GLOBALS = { };
+    function wrapFn(fn) {
+        return function () {
+            return fn.apply(this, arguments);
+        };
+    }
+    function wrapTimer(type, isInterval) {
+        GLOBALS[type] = global[type];
+        global[type] = function () {
+            var args = [ ], i = arguments.length;
+            while (i--) { args[i] = arguments[i]; }
+            
+            var ret = GLOBALS[type].apply(this, args);
+            var cbkey = timerCallback(ret);
+            
+            if (typeof ret[cbkey] === 'function') {
+                var stack = __stack, fnName = args[0].name;
+
+                for (var i = 2; i < stack.length; i++) {
                     if (/\//.test(stack[i].file)) {
-                        item[timerCallback(item)].__callSite = stack[i];
+                        ret[cbkey] = wrapFn(ret[cbkey]);
+                        ret[cbkey].__callSite = stack[i];
+                        ret[cbkey].__name = fnName || 'anonymous';
+                        ret[cbkey].__isInterval = isInterval;
                         break;
                     }
                 }
-            } else {
-                console.log('Uncertain what to do with item:', item);
             }
-        }
-        return _L_append.apply(this, arguments);
+
+            return ret;
+        };
     };
+    wrapTimer('setTimeout', false);
+    wrapTimer('setInterval', true);
 
     var EventEmitter = require('events').EventEmitter;
     var _EventEmitter_addListener = EventEmitter.prototype.addListener;
 
     EventEmitter.prototype.on =
-    EventEmitter.prototype.addListener = function (type, listener) {
+    EventEmitter.prototype.addListener = function (/*type, listener*/) {
         var stack = __stack;
-        listener.__fullStack = stack;
-        if (stack[3] && stack[3].file === 'events.js') {
-            listener.__callSite = stack[6]; // inline listener binding on net.connect
-        }
-        else if (stack[4] && /express\/lib\/application\.js$/.test(stack[4].file)) {
-            listener.__callSite = stack[5]; // express 4
-        }
-        else if (stack[3] && stack[3].file === 'http.js') {
-            listener.__callSite = stack[4]; // http.createServer(fn)
-        }
-        else {
-            listener.__callSite = stack[3];
+      
+        var args = [ ], i = arguments.length;
+        while (i--) { args[i] = arguments[i]; }
+        
+        if (typeof args[1] === 'function') {
+            var fnName = args[1].name, listener = wrapFn(args[1]);
+            listener.__fullStack = stack;
+          
+            for (var i = 2; i < stack.length; i++) {
+                if (/\//.test(stack[i].file)) {
+                    listener.__name = fnName || 'anonymous';
+                    listener.__callSite = stack[i];
+                    break;
+                }
+            }
+            
+            args[1] = listener;
         }
         
-        return _EventEmitter_addListener.apply(this, arguments);
+        return _EventEmitter_addListener.apply(this, args);
     };
 })();
 
 Socket = require('net').Socket;
 Server = require('net').Server;
+TlsServer = require('tls').Server;
+HttpServer = require('http').Server;
+HttpsServer = require('https').Server;
 Timer = process.binding('timer_wrap').Timer;
+dgramSocket = require('dgram').Socket;
+
+ChildProcess = (function () {
+    var ChildProcess = require('child_process').ChildProcess;
+
+    if (typeof ChildProcess !== 'function') {
+        // node 0.10 doesn't expose the ChildProcess constructor, so we have to get it on the sly
+        var cp = require('child_process').spawn('true', { stdio: 'ignore' });
+        ChildProcess = cp.constructor;
+    }
+    
+    return ChildProcess;
+})();
 
 function formatTime(t) {
     var labels = ['ms', 's', 'min', 'hr'],
@@ -94,17 +131,30 @@ function getCallsite(fn) {
     return fn.__callSite;
 };
 
+// some of the handles in older nodes (like 0.10) aren't functions
+// so instanceof will throw
+function instanceCheck(handle, cls) {
+    return Object.getPrototypeOf(handle) === cls;
+}
+
 function dump() {
     console.log('[WTF Node?] open handles:');
-    
-    var sockets = [ ], fds = [ ], servers = [ ], _timers = [ ], other = [ ];
+  
+    // sort the active handles into different types for logging
+    var sockets = [ ], fds = [ ], servers = [ ], _timers = [ ], processes = [ ], other = [ ];
     process._getActiveHandles().forEach(function (h) {
         if (h instanceof Socket) {
-          if (h.fd) { fds.push(h); }
-          else { sockets.push(h); }
+            // stdin, stdout, etc. are now instances of socket and get listed in open handles
+            // todo: a more specific/better way to identify them than the 'fd' property
+            if (h.fd) { fds.push(h); }
+            else { sockets.push(h); }
         }
         else if (h instanceof Server) { servers.push(h); }
+        else if (h instanceof dgramSocket) { servers.push(h); }
         else if (h instanceof Timer) { _timers.push(h); }
+        else if (h instanceof ChildProcess) { processes.push(h); }
+        
+        // catchall
         else { other.push(h); }
     });
     
@@ -118,17 +168,40 @@ function dump() {
             console.log(str);
         });
     }
+    
+    if (processes.length) {
+        console.log('- Child processes');
+        processes.forEach(function (cp) {
+            var fds = [ ];
+            console.log('  - PID %s', cp.pid);
+            
+            if (cp.stdio && cp.stdio.length) {
+                cp.stdio.forEach(function (s) {
+                    if (s._handle && s._handle.fd) { fds.push(s._handle.fd); }
+                    var idx = sockets.indexOf(s);
+                    if (idx > -1) {
+                        sockets.splice(idx, 1);
+                    }
+                });
+                console.log('    - STDIO file descriptors:', fds.join(', '));
+            }
+        });
+    }
 
     if (sockets.length) {
         console.log('- Sockets:');
         sockets.forEach(function (s) {
             if (s.destroyed) {
                 console.log('  - (?:?) -> %s:? (destroyed)', s._host);
-            } else {
+            } else if (s.localAddress) {
                 console.log('  - %s:%s -> %s:%s', s.localAddress, s.localPort, s.remoteAddress, s.remotePort);
+            } else if (s._handle && s._handle.fd) {
+                console.log('  - fd %s', s._handle.fd);
+            } else {
+                console.log('  - unknown socket');
             }
             var connectListeners = s.listeners('connect');
-            if (connectListeners) {
+            if (connectListeners && connectListeners.length) {
                 console.log('    - Listeners:');
                 connectListeners.forEach(function (fn) {
                     var callSite = getCallsite(fn);
@@ -141,28 +214,58 @@ function dump() {
     if (servers.length) {
         console.log('- Servers:');
         servers.forEach(function (s) {
-            var a = s.address();
-            console.log('  - %s:%s', a.address, a.port);
+            var type = 'unknown type';
+            if (s instanceof HttpServer) { type = 'HTTP'; }
+            else if (s instanceof HttpsServer) { type = 'HTTPS'; }
+            else if (s instanceof TlsServer) { type = 'TLS'; }
+            else if (s instanceof Server) { type = 'TCP'; }
+            else if (s instanceof dgramSocket) { type = 'UDP'; }
 
-            var connectListeners = s.listeners('connection');
-            if (connectListeners) {
+            try {
+                var a = s.address();
+            } catch (e) {
+                if (type === 'UDP') {
+                    // udp sockets that haven't been bound will throw, but won't prevent exit
+                    return;
+                }
+                throw e;
+            }
+          
+            
+            console.log('  - %s:%s (%s)', a.address, a.port, type);
+
+            var listeners = s.listeners('connection').map(function (fn) {
+                return { type: 'connection', fn: fn };
+            }).concat(s.listeners('request').map(function (fn) {
+                return { type: 'request', fn: fn };
+            })).concat(s.listeners('message').map(function (fn) {
+                return { type: 'message', fn: fn };
+            }));
+            
+            if (listeners && listeners.length) {
                 console.log('    - Listeners:');
-                connectListeners.forEach(function (fn) {
-                    var callSite = getCallsite(fn);
-                    console.log('      - %s: %s @ %s:%d', 'connection', fn.name || 'anonymous', callSite.file, callSite.line);
+                listeners.forEach(function (obj) {
+                    var callSite = getCallsite(obj.fn);
+                    console.log('      - %s: %s @ %s:%d', obj.type, obj.fn.__name || obj.fn.name || 'anonymous', callSite.file, callSite.line);
                 });
             }
         });
     }
     
-    var timers = [ ];
+    var timers = [ ], intervals = [ ];
     _timers.forEach(function (t) {
+        var timer = t._list, cb, cbkey;
         if (t._list) {
             // node v5ish behavior
-            var timer = t._list;
             do {
-                if (timerCallback(timer) && timers.indexOf(timer) === -1) {
-                    timers.push(timer);
+                cbkey = timerCallback(timer);
+                if (cbkey && timers.indexOf(timer) === -1) {
+                    cb = timer[cbkey];
+                    if (cb.__isInterval || cbkey === '_repeat') {
+                        intervals.push(timer);
+                    } else {
+                        timers.push(timer);
+                    }
                 }
                 timer = timer._idleNext;
             } while (!timer.constructor || timer !== t._list);
@@ -174,8 +277,14 @@ function dump() {
                     if (timer === t) {
                         break;
                     }
-                    if (timerCallback(timer) && timers.indexOf(timer) === -1) {
-                        timers.push(timer);
+                    cbkey = timerCallback(timer);
+                    if (cbkey && timers.indexOf(timer) === -1) {
+                        cb = timer[cbkey]
+                        if (cb.__isInterval) {
+                            intervals.push(timer);
+                        } else {
+                            timers.push(timer);
+                        }
                     }
                 }
                 
@@ -187,9 +296,27 @@ function dump() {
         console.log('- Timers:');
         
         timers.forEach(function (t) {
-            var fn = t._onTimeout,
+            var fn = t[timerCallback(t)],
                 callSite = getCallsite(fn);
-            console.log('  - (%d ~ %s) %s @ %s:%d', t._idleTimeout, formatTime(t._idleTimeout), fn.name, callSite.file, callSite.line);
+            if (fn.__name) {
+                console.log('  - (%d ~ %s) %s @ %s:%d', t._idleTimeout, formatTime(t._idleTimeout), fn.__name, callSite.file, callSite.line);
+            } else {
+                console.log('  - (%d ~ %s) %s @ %s:%d', t._idleTimeout, formatTime(t._idleTimeout), fn.name, callSite.file, callSite.line);
+            }
+        });
+    }
+    
+    if (intervals.length) {
+        console.log('- Intervals:');
+        
+        intervals.forEach(function (t) {
+            var fn = t[timerCallback(t)],
+                callSite = getCallsite(fn);
+            if (fn.__name) {
+                console.log('  - (%d ~ %s) %s @ %s:%d', t._idleTimeout, formatTime(t._idleTimeout), fn.__name, callSite.file, callSite.line);
+            } else {
+                console.log('  - (%d ~ %s) %s @ %s:%d', t._idleTimeout, formatTime(t._idleTimeout), fn.name, callSite.file, callSite.line);
+            }
         });
     }
     
---
+++ kitchensink.js
@@ -0,0 +1,60 @@
+var wtf = require('./index');
+
+var fs = require('fs'),
+    cp = require('child_process'),
+    net = require('net'),
+    tls = require('tls'),
+    http = require('http'),
+    https = require('https'),
+    dgram = require('dgram');
+
+function foo() { };
+
+function doStuff() {
+  // timers
+  setTimeout(foo, 1000);
+  setInterval(foo, 1000);
+  setTimeout(function inlineNamed() { }, 1000);
+  setTimeout(function () { }, 1000);
+
+  // servers
+  var httpServer = http
+      .createServer(function httpRequestListener() { }) 
+      .listen(function httpConnectionListener() { });
+
+  var httpsServer = https.createServer({
+      key: fs.readFileSync('./key.pem'),
+      cert: fs.readFileSync('./key-cert.pem')
+  }, function httpsRequestListener() { })
+      .listen(function httpsConnectionListener() { });
+  
+  var tcpServer = net.createServer(function netConnectionListener() { })
+      .listen(function netListenListener() { });
+
+  var tlsServer = tls.createServer({
+      key: fs.readFileSync('./key.pem'),
+      cert: fs.readFileSync('./key-cert.pem')
+  }, function tlsConnectionListener() { })
+      .listen(function tlsListenListener() { });
+
+  http.createServer();
+  net.createServer();
+
+  wtf.dump();
+  process.exit();
+}
+
+// child processes
+var proc = cp.spawn('cat');
+
+// udp servers
+var unboundUdpServer = dgram.createSocket('udp4');
+
+var udpServer = dgram.createSocket('udp4');
+udpServer.on('message', function udpMessageListener() { });
+udpServer.on('listening', function () {
+  // open socket
+  var socket = net.connect(80, 'www.google.com', doStuff);
+});
+
+udpServer.bind();
