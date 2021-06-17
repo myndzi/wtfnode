02adbce5ccc561cbd179ef409bb065da7dee68d8
Kris Reeves <kris@pressbuttonllc.com>
2021-06-16 20:33:52 -0400

    Isolate the stream tests

--- tests/test-once-flowing.js
+++ tests/test-once-flowing.js
@@ -5,9 +5,8 @@ var wtf = require('../index');
 var PassThrough = require('stream').PassThrough,
     assert = require('assert');
 
-var stream = new PassThrough();
-
 (function () {
+    var stream = new PassThrough();
     var count = 0;
     stream.once('data', function () {
         count++;
@@ -15,16 +14,17 @@ var stream = new PassThrough();
     stream.write('foo');
     stream.write('bar');
 
-    assert.equal(1, count, 'Handler should be called once');
+    assert.strictEqual(1, count, 'Handler should be called once, got '+count);
 })();
 
 (function () {
+    var stream = new PassThrough();
     var count = 0;
-    stream.on('data', function () {
+    stream.on('data', function testonceflowing(chunk) {
         count++;
     });
     stream.write('foo');
     stream.write('bar');
 
-    assert.equal(2, count, 'Handler should be called twice');
+    assert.strictEqual(2, count, 'Handler should be called twice, got '+count);
 })();
