f01d87ebd3b3b5c1096589b3e10b7814e6cc6562
Kris Reeves <kris@pressbuttonllc.com>
2021-07-14 19:15:40 -0400

    This test should work if source-map-support is not available

--- tests/test-no-source-map-support.js
+++ tests/test-no-source-map-support.js
@@ -1,7 +1,11 @@
 // we set the module's export to the same value that we use to indicate
 // "we failed to require it", to verify that nothing breaks even when it's present
-require('source-map-support');
-require.cache[require.resolve('source-map-support')] = false;
+try {
+    require('source-map-support');
+    require.cache[require.resolve('source-map-support')] = false;
+} catch (e) {
+
+}
 
 var wtf = require('../index');
 
